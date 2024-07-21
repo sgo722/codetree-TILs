f9c807586cdc687b8be06d43f8392c4b354acc8f
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-07-21 18:47:30 +0900

    Title: 자리 차지하기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240721/자리 차지하기/take-place.java
+++ 240721/자리 차지하기/take-place.java
@@ -20,24 +20,20 @@ public class Main{
             list.add(number);
         }
 
-        dfs(0);
-        System.out.println(ans);
-    }
-
-    static void dfs(int index){
-        if(index == n){
-            return;
+        for(int i=1; i<=m; i++){
+            ts.add(i);
         }
 
-        int number = list.get(index);
-        for(int i=number; i>=1; i--){
-            if(!ts.contains(i)) {
-                ts.add(i);
-                dfs(index + 1);
-                ans = Math.max(ans, index+1);
-                break;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            Integer number = list.get(i);
+            Integer floor = ts.floor(number);
+            if(floor == null){
+                System.out.println(count);
+                return;
             }
+            ts.remove(floor);
+            count++;
         }
-
     }
 }
