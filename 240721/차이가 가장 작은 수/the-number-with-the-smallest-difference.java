377c56a73013bbdb3b845c522173de67f1c10582
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-07-21 19:00:25 +0900

    Title: 차이가 가장 작은 수, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240721/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static TreeSet<Integer> ts;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ts = new TreeSet<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int firstNumber = Integer.parseInt(br.readLine());
+        if(n == 1){
+            System.out.println(-1);
+            return;
+        }
+        ts.add(firstNumber);
+        int diff = Integer.MAX_VALUE;
+        for(int i=1; i<n; i++){
+            int number = Integer.parseInt(br.readLine());
+            Integer ceiling = ts.ceiling(number);
+            Integer floor = ts.floor(number);
+            if(ceiling != null){
+                int CurDiff = ceiling - number;
+                if(CurDiff >= m) {
+                    diff = Math.min(diff, CurDiff);
+                }
+            }
+            if(floor != null){
+                int CurDiff = number - floor;
+                if(CurDiff >= m) {
+                    diff = Math.min(diff, CurDiff);
+                }
+            }
+            ts.add(number);
+        }
+
+        System.out.println(diff);
+    }
+}
