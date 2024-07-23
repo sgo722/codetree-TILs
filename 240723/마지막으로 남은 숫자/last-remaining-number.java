05003ceec799ea315a7bfef42ce617a80ca76ae8
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-07-23 21:08:10 +0900

    Title: 마지막으로 남은 숫자, Time: 217ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240723/마지막으로 남은 숫자/last-remaining-number.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> o2 - o1);
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+
+        while(pq.size() >= 2){
+            Integer a = pq.poll();
+            Integer b = pq.poll();
+            int gap = Math.abs(a - b);
+            if(gap == 0) continue;
+            pq.add(gap);
+        }
+
+        System.out.println(pq.peek());
+
+    }
+}
