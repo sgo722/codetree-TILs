8ac6830aa631dcc9856cb0627b945011168f837a
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-08-19 17:55:35 +0900

    Title: 점 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240819/점 개수 세기/count-number-of-points.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    static int n,q;
+
+    static TreeSet<Integer> treeSet;
+
+    static TreeMap<Integer, Integer> treeMap;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        treeSet = new TreeSet<>();
+        treeMap = new TreeMap<>();
+
+        n = sc.nextInt();
+        q = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            int number = sc.nextInt();
+            treeSet.add(number);
+        }
+
+        int count = 1;
+        for(int number : treeSet){
+            treeMap.put(number, count);
+            count++;
+        }
+
+        for(int i=0; i<q; i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+
+            Integer ceiling = treeSet.ceiling(s);
+            Integer floor = treeSet.floor(e);
+            Integer start = treeMap.get(ceiling);
+            Integer end = treeMap.get(floor);
+
+            System.out.println(end - start + 1);
+        }
+
+
+    }
+}
